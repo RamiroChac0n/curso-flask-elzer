901634fdeec28d622c4366ccf93bff28d6621b9a
Ramiro Chacón <ramiro20010202@gmail.com>
2024-12-10 16:04:28 -0600

    hello world

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_restx import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@api.route('/hello')
+class hello_world(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
